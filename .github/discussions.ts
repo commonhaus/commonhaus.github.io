d0dc4542a53a63b236307c19698e040f76fcbe10
Erin Schnabel <erinschnabel@gmail.com>
2024-01-03 14:25:16 -0500

    Lume + Deno; GH Discussions; Foundation bylaws & policies

--- .github/discussions.ts
+++ .github/discussions.ts
@@ -2,15 +2,6 @@ import { ensureDirSync } from "https://deno.land/std@0.114.0/fs/mod.ts";
 import { join } from "https://deno.land/std@0.114.0/path/mod.ts";
 import { safeLoad, safeDump } from "https://deno.land/x/js_yaml_port@3.14.0/js-yaml.js";
 
-interface Comment {
-  id: string;
-  body: string;
-  author: {
-    login: string;
-  };
-  createdAt: string;
-}
-
 interface Discussion {
   id: string;
   title: string;
@@ -25,6 +16,13 @@ interface Discussion {
   url: string;
   body: string;
   createdAt: string;
+  updatedAt: string;
+  closed: boolean;
+  closedAt: string;
+  isAnswered: boolean;
+  answerChosenAt: string;
+  locked: boolean;
+  activeLockReason: string;
 }
 
 interface DiscussionsData {
@@ -37,45 +35,62 @@ interface DiscussionsData {
   };
 }
 
-function filterDiscussionsByCategory(data: DiscussionsData, categoryName: string): Discussion[] {
+function filterDiscussionsByCategory(data: DiscussionsData, category: string): Discussion[] {
   return data.data.repository.discussions.nodes.filter(
-    (discussion) => discussion.category.name.toLowerCase() === categoryName
+    (discussion) => discussion.category.name.toLowerCase() === category
   );
 }
 
-function slugifyTitle(title: string): string {
-  // Remove emoji
-  const titleWithoutEmoji = title.replace(/[^\w\s\p{P}]/gu, "");
+const prefixMap: Record<string, string> = {
+  announcements: '[📣 ]',
+  reviews: '[🗳️ ]',
+};
 
-  // Split at first punctuation or first 4 words
-  const splitTitle = titleWithoutEmoji.split(/[\.;,](?=\s)/);
-  const words = titleWithoutEmoji.trim().split(/\s+/);
-  const shortTitle = splitTitle.length == 0
-      ? words.slice(0, 4).join(' ')
-      : splitTitle[0];
+function generateDiscussionContent(discussion: Discussion, id: string, category: string): string {
+  const title = discussion.title.replace(/"/g, '\\"')
+  const prefix = prefixMap[category];
+  let updated = '';
+  if (discussion.updatedAt && discussion.updatedAt !== discussion.createdAt) {
+    updated = `\nupdated: ${discussion.updatedAt}`;
+  }
 
-  // Slugify the result
-  return shortTitle.toLowerCase().replace(/\s+/g, '-').replace(/[^\w-]+/g, '');
+  let status = '';
+  let data = `---
+title: "${prefix} ${title}"
+github: "${discussion.url}"
+author: "${discussion.author.login}"
+date: ${discussion.createdAt}${updated}
+url: "/posts/${id}.html"
+tags:
+- ${category}`;
+
+if (discussion.closed) {
+  data += `\n- closed"`;
+  status += `\n  closedAt: "${discussion.closedAt}"`
+}
+if (discussion.locked) {
+  data += `\n- locked"`;
+  status += `\n  lockReason: "${discussion.activeLockReason}"`
+}
+if (discussion.isAnswered) {
+  data += `\n- answered"`;
+  status += `\n  answeredAt: "${discussion.answerChosenAt}"`;
+}
+  // Add conditional data
+  if (status) {
+    data += `\nstatus:${status}`;
+  }
+  data += `\n---\n${discussion.body}\n`;
+  console.log(data);
+  return data;
 }
 
-function generateDiscussionContent(discussion: Discussion, slug: string): string {
-  return `---
-title: ${discussion.title}
-github: ${discussion.url}
-author: ${discussion.author.login}
-date: ${discussion.createdAt}
-url: /announcements/${slug}.html
----
-${discussion.body}
-`;
-}
-
-function writeDiscussionsToFiles(discussions: Discussion[], outputDir: string): void {
+function writeDiscussionsToFiles(discussions: Discussion[], outputDir: string, category: string): void {
   ensureDirSync(outputDir);
   for (const discussion of discussions) {
     const id = discussion.url.substring(discussion.url.lastIndexOf('/') + 1);
-    const slug = id + slugifyTitle(discussion.title);
-    const content = generateDiscussionContent(discussion, slug);
+    // const slug = id + slugifyTitle(discussion.title);
+    const content = generateDiscussionContent(discussion, id, category);
     const filePath = join(outputDir, `${id}.md`);
     Deno.writeTextFileSync(filePath, content);
   }
@@ -98,13 +113,17 @@ function updateAuthors(discussions: Discussion[]): void {
   Deno.writeTextFileSync(authorsPath, safeDump(authors));
 }
 
+function updateCategory(data: DiscussionsData, category: string) {
+  // Filter discussions by category (Announcements),
+  const discussions = filterDiscussionsByCategory(data, category);
+  writeDiscussionsToFiles(discussions, `./site/posts`, category);
+  // Update authors.yml (add/update author of last 10 discussions)
+  updateAuthors(discussions);
+}
+
 // Usage:
-// First, use a deno task to download the discussions from GitHub > discussions.json
+// First, use a deno task to download the last 10 discussions from GitHub > discussions.json
 // Parse the discussions.json file
 const data: DiscussionsData = JSON.parse(Deno.readTextFileSync('discussions.json'));
-// Filter discussions by category (Announcements)
-const discussions = filterDiscussionsByCategory(data, 'announcements');
-// Write markdown for discussions by id (will replace, we're only fetching 10)
-writeDiscussionsToFiles(discussions, './site/announcements');
-// Update authors.yml (add/update author of last 10 discussions)
-updateAuthors(discussions);
+updateCategory(data, 'announcements');
+updateCategory(data, 'reviews');
--- _config.ts
+++ _config.ts
@@ -1,8 +1,10 @@
 import lume from "lume/mod.ts";
-import { Page } from "lume/core/file.ts";
+import { Data, Page } from "lume/core/file.ts";
+import { safeLoad } from "https://deno.land/x/js_yaml_port@3.14.0/js-yaml.js";
 
 import date from "lume/plugins/date.ts";
 import favicon from "lume/plugins/favicon.ts";
+import feed from "lume/plugins/feed.ts";
 import metas from "lume/plugins/metas.ts";
 import resolveUrls from "lume/plugins/resolve_urls.ts";
 import sass from "lume/plugins/sass.ts";
@@ -12,6 +14,8 @@ import slugify_urls from "lume/plugins/slugify_urls.ts";
 import anchor from "npm:markdown-it-anchor";
 import footnote from "npm:markdown-it-footnote";
 import callouts from "npm:markdown-it-obsidian-callouts";
+import { load } from "https://deno.land/std@0.208.0/yaml/_loader/loader.ts";
+import { customByTagNameSym } from "https://deno.land/x/deno_dom@v0.1.43/src/dom/selectors/custom-api.ts";
 
 const markdown = {
     options: {
@@ -32,6 +36,8 @@ const site = lume({
     prettyUrls: false,
 }, { markdown });
 
+site.ignore("foundation/node_modules", "foundation/.git", "foundation/.github", "foundation/.pandoc", "foundation/templates");
+
 // Copy the content of "static" directory to the root of your site
 site.copy("static", "/");
 
@@ -45,28 +51,36 @@ site
             "@": "",
         },
     }))
+    .use(feed({
+        output: ["/posts.rss", "/posts.json"],
+        query: "post",
+        limit: 10,
+        info: {
+            title: "=metas.site",
+            description: "=description",
+        },
+        items: {
+            title: "=title",
+            published: "=date",
+            updated: "=status.updated",
+        },
+    }))
     .use(sass({
         includes: "_includes/scss",
     }))
     .use(sitemap({
         query: "indexable=true",
-    // }))
-    // favicon.svg file must have 1/1 aspect ratio
-    // https://lume.land/plugins/favicon/
-    // .use(favicon({
-    //     input: "/my-custom-favicon.png",
+        // }))
+        // favicon.svg file must have 1/1 aspect ratio
+        // https://lume.land/plugins/favicon/
+        // .use(favicon({
+        //     input: "/my-custom-favicon.png",
     }));
 
-site.data("description",
-`The Commonhaus Foundation is dedicated to nurturing and sustaining GitHub-style
-open-source projects through community-driven collaboration and shared stewardship.
-We unite developers, contributors, and users in a dynamic ecosystem, focusing on
-the long-term growth and stability of essential open-source libraries and frameworks
-without the overhead of traditional governance models.`.replace(/\n/g, ' '));
-
-site.data("layout", "layouts/page.vto");
-site.data("layout", "layouts/bylaws.vto", "/foundation");
-
+// Update URLs for foundation pages (third parameter)
+// If it isn't markdown, or is a template, disable it (return false)
+// If it is a README, remove readme.html from the URL (index.html instead)
+// Otherwise, just return the URL
 site.data("url", (page: Page) => {
     if (page.src.ext !== ".md" || page.src.path.indexOf("templates") >= 0) {
         return false;
@@ -77,12 +91,37 @@ site.data("url", (page: Page) => {
     return page.data.url;
 }, "/foundation");
 
+// deno-lint-ignore no-explicit-any
+const FOUNDATION_DATA: Record<string, any> = safeLoad(Deno.readTextFileSync("./site/_includes/foundation.yml"));
+
 site.preprocess([".html"], (pages) => {
     for (const page of pages) {
         // For foundation pages:
-        // If the title hasn't been set, set it to the first H1 in the content
-        // Add the link to the github page based on the src path
         if (page.src.path.startsWith("/foundation")) {
+
+            // deno-lint-ignore no-explicit-any
+            let structure: Record<string, any> | undefined = FOUNDATION_DATA;
+
+            // Populate page metadata from the side file
+            const keys = page.src.path.substring(1).replace("foundation/", "").split("/");
+            for (const key of keys) {
+                if (structure && structure[key] !== undefined) {
+                    structure = structure[key];
+                } else {
+                    structure = undefined;
+                    break
+                }
+            }
+            if (structure) {
+                page.data = { ...page.data, ...structure };
+                page.data.tags = page.data.tags || [];
+                if (keys.length > 1) {
+                    page.data.tags.push(keys[0]);
+                }
+            }
+
+            // If the title hasn't been set, set it to the first H1 in the content
+            // Add the link to the github page based on the src path
             if (!page.data.title) {
                 const content = page.data.content as string;
                 const match = content.match(/#\s(.*)$/m); // 'm' flag for multiline
@@ -94,7 +133,18 @@ site.preprocess([".html"], (pages) => {
             }
             page.data.github = page.src.entry?.path.replace("/foundation/", "https://github.com/commonhaus/foundation-draft/blob/main/");
         }
+
+        if (page.data.date && page.data.updated) {
+           if (page.data.date.getFullYear() == page.data.updated.getFullYear()
+                && page.data.date.getMonth() == page.data.updated.getMonth()
+                && page.data.date.getDate() == page.data.updated.getDate()) {
+                page.data.updated = undefined;
+            } else {
+                console.log("Same-day revision", page.src.path, page.data.date, page.data.updated);
+            }
+        }
     }
 });
+
 export default site;
 
