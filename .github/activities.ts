6a1eb4539eb207cfd9a0c968b6e3dd7a3d4d7529
Erin Schnabel <erinschnabel@gmail.com>
2024-03-08 11:24:25 -0500

    ✨ Create vote result pages and svg

--- .github/activities.ts
+++ .github/activities.ts
@@ -2,86 +2,124 @@ import { ensureDirSync } from "https://deno.land/std@0.114.0/fs/mod.ts";
 import { join } from "https://deno.land/std@0.114.0/path/mod.ts";
 import { safeLoad, safeDump } from "https://deno.land/x/js_yaml_port@3.14.0/js-yaml.js";
 
+interface Problem {
+    path: string[];
+    explanation: string;
+}
+interface Error {
+    message: string;
+    extensions?: {
+        "value": string;
+        "problems": Problem[];
+    },
+    locations?: {
+        line: number;
+        column: number;
+    }[];
+}
 interface Label {
-  name: string;
+    name: string;
 }
 interface Author {
-  login: string;
-  url: string;
-  avatarUrl: string;
+    login: string;
+    url: string;
+    avatarUrl: string;
 }
 interface ItemWithAuthor {
-  id: string;
-  title: string;
-  number: number;
-  author: Author;
-  labels: {
-    nodes: Label[];
-  };
-  url: string;
-  body: string;
-  createdAt: string;
-  updatedAt: string;
-  closed: boolean;
-  closedAt: string;
-  locked: boolean;
-  activeLockReason: string;
-  category?: {
-    name: string;
-  };
-  isAnswered?: boolean;
-  answerChosenAt?: string;
+    id: string;
+    title: string;
+    number: number;
+    author: Author;
+    labels: {
+        nodes: Label[];
+    };
+    url: string;
+    body: string;
+    createdAt: string;
+    updatedAt: string;
+    closed: boolean;
+    closedAt: string;
+    locked: boolean;
+    activeLockReason: string;
+    category?: {
+        name: string;
+    };
+    isAnswered?: boolean;
+    answerChosenAt?: string;
 }
 interface DiscussionsData {
-  data: {
-    repository: {
-      discussions: {
-        nodes: ItemWithAuthor[];
-      };
+    errors?: Error[];
+    data: {
+        repository: {
+            discussions: {
+                nodes: ItemWithAuthor[];
+            };
+        };
     };
-  };
 }
 interface PullRequestData {
-  data: {
-    repository: {
-      pullRequests: {
-        nodes: ItemWithAuthor[];
-      };
+    errors?: Error[];
+    data: {
+        repository: {
+            pullRequests: {
+                nodes: ItemWithAuthor[];
+            };
+        };
     };
-  };
 }
 interface PinnedItem {
-  discussion: {
-    id: string;
-  };
+    discussion: {
+        id: string;
+    };
 }
 interface PinnedItemData {
-  data: {
-    repository: {
-      pinnedDiscussions: {
-        nodes: PinnedItem[];
-      };
+    errors?: Error[];
+    data: {
+        repository: {
+            pinnedDiscussions: {
+                nodes: PinnedItem[];
+            };
+        };
     };
-  };
 }
 
 const prefixMap: Record<string, string> = {
-  announcements: '[📣  ]',
-  reviews: '[🗳️  ]',
+    announcements: '[📣  ]',
+    reviews: '[🗳️  ]',
 };
 
-const pins: PinnedItemData = JSON.parse(Deno.readTextFileSync('./site/activity/activity-pinned.json'));
-const pinnedIds = pins.data.repository.pinnedDiscussions.nodes
-    .map((node) => node.discussion.id);
+// Get last commit date for a file
+function runGraphQL(filePath: string): string {
+    const command = new Deno.Command('gh', {
+        args: [
+            'api', 'graphql',
+            '-F', "owner=commonhaus",
+            '-F', "name=foundation-draft",
+            '-F', `query=@${filePath}`,
+        ]
+    });
 
+    const { code, stdout, stderr } = command.outputSync();
+    const output = new TextDecoder().decode(stdout).trim();
+    console.log(code, filePath, new TextDecoder().decode(stderr));
+    console.assert(code === 0);
+    return output;
+}
+
+const pins: PinnedItemData = JSON.parse(runGraphQL('.github/graphql/query.pinned.graphql'));
+if (pins.errors || !pins.data) {
+    console.error(pins);
+    Deno.exit(1);
+}
+
+const pinnedIds = pins.data.repository.pinnedDiscussions.nodes.map((node) => node.discussion.id);
 
 function generateContent(item: ItemWithAuthor, id: string, type: string): string {
-  const title = item.title.replace(/"/g, '\\"')
-  const prefix = item.category
-    ? prefixMap[item.category.name.toLowerCase()]
-    : '';
-  let status = '';
-  let data = `---
+    const title = item.title.replace(/"/g, '\\"')
+    const prefix = item.category
+        ? prefixMap[item.category.name.toLowerCase()]
+        : '';
+    let data = `---
 title: "${title}"
 rss-title: "${prefix}${title}"
 github: "${item.url}"
@@ -93,83 +131,94 @@ url: "/activity/${id}.html"
 type: ${type}
 tags:
 - post`;
-  if (item.category) {
-    data += `\n- ${item.category.name.toLowerCase()}`;
-  }
-  if (item.closed) {
-    data += `\nclosedAt: ${item.closedAt}`
-  }
-  if (item.locked) {
-    data += `\nlockReason: "${item.activeLockReason}"`
-  }
-  if (item.isAnswered) {
-    data += `\nansweredAt: ${item.answerChosenAt}`;
-  }
-  if (pinnedIds.includes(item.id)) {
-    data += `\npinned: true`;
-  }
-  data += `\n---\n${item.body}\n`;
-  // console.log(data);
-  return data;
+    if (item.category) {
+        data += `\n- ${item.category.name.toLowerCase()}`;
+    }
+    if (item.closed) {
+        data += `\nclosedAt: ${item.closedAt}`
+    }
+    if (item.locked) {
+        data += `\nlockReason: "${item.activeLockReason}"`
+    }
+    if (item.isAnswered) {
+        data += `\nansweredAt: ${item.answerChosenAt}`;
+    }
+    if (pinnedIds.includes(item.id)) {
+        data += `\npinned: true`;
+    }
+    data += `\n---\n${item.body}\n`;
+    // console.log(data);
+    return data;
 }
 
 function writeItemsToFiles(items: ItemWithAuthor[], outputDir: string, type: string): void {
-  ensureDirSync(outputDir);
-  for (const item of items) {
-    const id = item.number.toString();
-    // const slug = id + slugifyTitle(discussion.title);
-    const content = generateContent(item, id, type);
-    const filePath = join(outputDir, `${id}.md`);
-    Deno.writeTextFileSync(filePath, content);
-  }
+    ensureDirSync(outputDir);
+    for (const item of items) {
+        const id = item.number.toString();
+        const content = generateContent(item, id, type);
+        const filePath = join(outputDir, `${id}.md`);
+        Deno.writeTextFileSync(filePath, content);
+    }
 }
 
 function updateAuthors(items: ItemWithAuthor[]): void {
-  const authorsPath = './site/_data/authors.yml';
-  const authorsYaml = Deno.readTextFileSync(authorsPath);
-  const authors = safeLoad(authorsYaml) || {};
-  for (const item of items) {
-    const author = item.author;
-    if (!authors[author.login]) {
-      authors[author.login] = {
-        login: author.login,
-        url: author.url,
-        avatar: author.avatarUrl,
-      };
+    const authorsPath = './site/_data/authors.yml';
+    const authorsYaml = Deno.readTextFileSync(authorsPath);
+    const authors = safeLoad(authorsYaml) || {};
+    for (const item of items) {
+        const author = item.author;
+        if (!authors[author.login]) {
+            authors[author.login] = {
+                login: author.login,
+                url: author.url,
+                avatar: author.avatarUrl,
+            };
+        }
     }
-  }
-  Deno.writeTextFileSync(authorsPath, safeDump(authors));
+    Deno.writeTextFileSync(authorsPath, safeDump(authors));
 }
 
 function updateDiscussions(data: DiscussionsData) {
-  // Filter discussions by category (Announcements) or label (Notice),
-  const discussions = data.data.repository.discussions.nodes
-    .filter((discussion: ItemWithAuthor) =>
-      (discussion.category && prefixMap[discussion.category.name.toLowerCase()])
-      || discussion.labels.nodes.some((label: Label) => label.name.toLowerCase() === 'notice')
-    );
-  writeItemsToFiles(discussions, `./site/activity`, "discussion");
-  // Update authors.yml (add/update author of last 10 discussions)
-  updateAuthors(discussions);
+    // Filter discussions by category (Announcements) or label (Notice),
+    const discussions = data.data.repository.discussions.nodes
+        .filter((discussion: ItemWithAuthor) =>
+            (discussion.category && prefixMap[discussion.category.name.toLowerCase()])
+            || discussion.labels.nodes.some((label: Label) => label.name.toLowerCase() === 'notice')
+        );
+
+    writeItemsToFiles(discussions, `./site/activity`, "discussion");
+
+    // Update authors.yml (add/update author of last 10 discussions)
+    updateAuthors(discussions);
 }
 
 function updatePullRequests(data: PullRequestData) {
-  // Filter discussions by label (Notice),
-  const prs = data.data.repository.pullRequests.nodes
-    .filter((pr: ItemWithAuthor) =>
-      pr.labels.nodes.some((label: Label) => label.name.toLowerCase() === 'notice')
-    );
-  prs.forEach((pr: ItemWithAuthor) => pr.category = { name: 'reviews' });
-  writeItemsToFiles(prs, `./site/activity`, "pullRequest");
-  // Update authors.yml (add/update author of last 10 discussions)
-  updateAuthors(prs);
+    // Filter discussions by label (Notice),
+    const prs = data.data.repository.pullRequests.nodes
+        .filter((pr: ItemWithAuthor) =>
+            pr.labels.nodes.some((label: Label) => label.name.toLowerCase() === 'notice')
+        );
+
+    prs.forEach((pr: ItemWithAuthor) => pr.category = { name: 'reviews' });
+    writeItemsToFiles(prs, `./site/activity`, "pullRequest");
+
+    // Update authors.yml (add/update author of last 10 discussions)
+    updateAuthors(prs);
 }
 
 // Usage:
 // First, use a deno task to download the last 10 discussions from GitHub > discussions.json
 // Parse the discussions.json file
-const discuss: DiscussionsData = JSON.parse(Deno.readTextFileSync('./site/activity/activity-discussions.json'));
+const discuss: DiscussionsData = JSON.parse(runGraphQL('.github/graphql/query.discussions.graphql'));
+if (discuss.errors || !discuss.data) {
+    console.error(discuss);
+    Deno.exit(1);
+}
 updateDiscussions(discuss);
 
-const prs: PullRequestData = JSON.parse(Deno.readTextFileSync('./site/activity/activity-prs.json'));
+const prs: PullRequestData = JSON.parse(runGraphQL('.github/graphql/query.pullrequests.graphql'));
+if (prs.errors || !prs.data) {
+    console.error(prs);
+    Deno.exit(1);
+}
 updatePullRequests(prs);
---
+++ .github/votes.ts
@@ -0,0 +1,191 @@
+import { ensureDirSync } from "https://deno.land/std@0.114.0/fs/mod.ts";
+
+interface Problem {
+    path: string[];
+    explanation: string;
+}
+interface Error {
+    message: string;
+    extensions?: {
+        "value": string;
+        "problems": Problem[];
+    },
+    locations?: {
+        line: number;
+        column: number;
+    }[];
+}
+interface Label {
+    name: string;
+}
+interface Item {
+    id: string;
+    title: string;
+    number: number;
+    closed: boolean;
+    closedAt: string;
+    labels: {
+        nodes: Label[];
+    };
+    repository: {
+        nameWithOwner: string;
+    }
+    url: string;
+}
+interface Result {
+    errors?: Error[];
+    data?: {
+        node: {
+            discussion?: Item,
+            issue?: Item,
+            body: string;
+            createdAt: string;
+            id: string;
+            updatedAt: string;
+            url: string;
+        };
+    };
+}
+interface Voter {
+    login: string;
+    url: string;
+    avatarUrl: string;
+}
+interface VoteCategory {
+    reactions: string[];
+    team: Voter[];
+    teamTotal: number;
+    total: number;
+}
+interface DuplicateVote {
+    user: Voter;
+    createdAt: string;
+    reaction: string;
+}
+interface VoteData {
+    voteType: string;
+    hasQuorum: boolean;
+    group: string;
+    groupSize: number;
+    groupVotes: number;
+    countedVotes: number;
+    droppedVotes: number;
+    votingThreshold: string;
+    categories?: Record<string, VoteCategory>;
+    duplicates: DuplicateVote[]
+    missingGroupActors: Voter[];
+
+    // Fields are added (or modified) by this script for rendering
+    closed?: boolean;
+    closedAt?: string;
+    commentId?: string;
+    date?: string;
+    github?: string;
+    ignored?: VoteCategory;
+    itemId?: string;
+    itemTitle?: string;
+    number?: number;
+    repoName?: string;
+    sortedCategories?: [string, VoteCategory][];
+    tags?: string[];
+    type?: string;
+    updated?: string;
+    url?: string;
+}
+
+// Get last commit date for a file
+function runGraphQL(commentId: string, filePath: string): string {
+    const command = new Deno.Command('gh', {
+        args: [
+            'api', 'graphql',
+            '-F', "owner=commonhaus",
+            '-F', "name=foundation-draft",
+            '-F', `commentId=${commentId}`,
+            '-F', `query=@${filePath}`,
+        ]
+    });
+
+    const { code, stdout, stderr } = command.outputSync();
+    const output = new TextDecoder().decode(stdout).trim();
+    console.log(code, filePath, new TextDecoder().decode(stderr));
+    console.log(output);
+    console.assert(code === 0);
+    return output;
+}
+
+const commentId = Deno.args[0];
+console.log(`Check vote data in comment ${commentId}`);
+
+const result: Result = JSON.parse(runGraphQL(commentId, '.github/graphql/query.comment.graphql'));
+console.log(result);
+
+// If we have errors, we're done.
+if (result.errors || !result.data) {
+    console.error(result);
+    Deno.exit(1);
+}
+// If we can't find the parent item, we're done
+const comment = result.data.node;
+const item = comment.discussion || comment.issue;
+if (!item) {
+    console.error(comment);
+    Deno.exit(1);
+}
+
+const match = comment.body.match(/<!-- vote::data ([\s\S]*?)-->/);
+const voteData: VoteData = JSON.parse(match ? match[1].trim() : '');
+
+voteData.commentId = comment.id;
+voteData.github = item.url;
+voteData.itemId = item.id;
+voteData.itemTitle = item.title;
+voteData.number = item.number;
+voteData.repoName = item.repository.nameWithOwner;
+voteData.type = 'vote';
+voteData.date = comment.createdAt;
+voteData.updated = comment.updatedAt ? comment.updatedAt : comment.createdAt;
+voteData.url = `/votes/${item.repository.nameWithOwner}/${item.number}.html`;
+
+voteData.tags = item.labels.nodes.map(label => label.name);
+voteData.tags.push('vote');
+voteData.closed = item.closed;
+if (item.closed) {
+    voteData.tags.push('closed');
+    voteData.closedAt = item.closedAt;
+}
+
+if (voteData.categories) {
+    const ignored: VoteCategory | undefined = voteData.categories['ignored'];
+    if (ignored) {
+        voteData.ignored = ignored;
+        delete voteData.categories['ignored'];
+    }
+
+    for (const category of Object.values(voteData.categories)) {
+        // +1, -1, laugh, confused, heart, hooray, rocket, eyes
+        // thumbs_up, plus_one, thumbs_down, minus_one
+        category.reactions = category.reactions.map((r: string) =>
+            r.replace('+1', '👍')
+                .replace('thumbs_up', '👍')
+                .replace('plus_one', '👍')
+                .replace('-1', '👎')
+                .replace('thumbs_down', '👎')
+                .replace('minus_one', '👎')
+                .replace('laugh', '😄')
+                .replace('confused', '😕')
+                .replace('heart', '❤️')
+                .replace('hooray', '🎉')
+                .replace('rocket', '🚀')
+                .replace('eyes', '👀')
+        );
+    }
+
+    voteData.sortedCategories = Object.entries(voteData.categories).sort();
+}
+
+// The relative path for the vote file
+const relativeVotePath = `./site/votes/${item.repository.nameWithOwner}/`;
+const fileName = `${item.number}.json`;
+console.log(`Writing vote data to ${relativeVotePath}${fileName}`);
+ensureDirSync(relativeVotePath);
+Deno.writeTextFileSync(`${relativeVotePath}${fileName}`, JSON.stringify(voteData, null, 2));
