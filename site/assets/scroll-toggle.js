0052de3b42942b81747f03a455c44a8341631131
Erin Schnabel <erinschnabel@gmail.com>
2024-03-16 12:51:48 -0400

    Reference images; minify html output

--- site/assets/scroll-toggle.js
+++ site/assets/scroll-toggle.js
@@ -1,1 +1,32 @@
-document.addEventListener("DOMContentLoaded",(function(){const e=document.querySelector("header.site"),t=document.querySelector("ul.top-level"),n=window.matchMedia("(min-width: 650px)");function o(n){n.matches?t.style.height="auto":t.style.height=globalThis.innerHeight-e.offsetHeight+"px"}o(n),n.addEventListener("change",o),document.getElementById("menu-toggle").addEventListener("change",(function(){this.checked?(document.body.classList.add("no-scroll"),t.style.height=globalThis.innerHeight-e.offsetHeight+"px"):document.body.classList.remove("no-scroll")}))}));
+document.addEventListener('DOMContentLoaded', function () {
+    const header = document.querySelector('header.site');
+    const menu = document.querySelector('ul.top-level');
+    const mql = globalThis.matchMedia(`(min-width: ${menuSnap}px)`);
+    const menuSnap = 650;
+
+    function checkViewport(e) {
+        if (e.matches) {
+            // If the viewport is at least menuSnap pixels wide
+            menu.style.height = 'auto';
+        } else {
+            // If the viewport is less than menuSnap pixels wide
+            menu.style.height = (globalThis.innerHeight - header.offsetHeight) + 'px';
+        }
+    }
+
+    // Call the function at run time
+    checkViewport(mql);
+    // Attach listener function on state changes
+    mql.addEventListener('change', checkViewport);
+
+    document.getElementById('menu-toggle').addEventListener('change', function () {
+        if (this.checked) {
+            document.body.classList.add('no-scroll');
+            menu.style.height = (globalThis.innerHeight - header.offsetHeight) + 'px';
+            console.log('checked', document.body.classList, menu.style.height);
+        } else {
+            document.body.classList.remove('no-scroll');
+            console.log('unchecked', document.body.classList);
+        }
+    });
+});
