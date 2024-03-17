3de01f7590fff4c9445bc7634c7d2a4b10316edf
Erin Schnabel <erinschnabel@gmail.com>
2024-03-16 21:54:21 -0400

    mobile menus are the worst.

--- site/assets/scroll-toggle.js
+++ site/assets/scroll-toggle.js
@@ -1,19 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
     const header = document.querySelector('header.site');
     const menu = document.querySelector('ul.top-level');
-    const mql = globalThis.matchMedia(`(min-width: ${menuSnap}px)`);
     const menuSnap = 650;
 
     function checkViewport(e) {
         if (e.matches) {
             // If the viewport is at least menuSnap pixels wide
             menu.style.height = 'auto';
+            menu.style.top = header.offsetHeight + 'px';
         } else {
             // If the viewport is less than menuSnap pixels wide
             menu.style.height = (globalThis.innerHeight - header.offsetHeight) + 'px';
+            menu.style.top = header.offsetHeight + 'px';
         }
     }
 
+    const mql = window.matchMedia(`(min-width: ${menuSnap}px)`);
     // Call the function at run time
     checkViewport(mql);
     // Attach listener function on state changes
@@ -22,11 +24,8 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('menu-toggle').addEventListener('change', function () {
         if (this.checked) {
             document.body.classList.add('no-scroll');
-            menu.style.height = (globalThis.innerHeight - header.offsetHeight) + 'px';
-            console.log('checked', document.body.classList, menu.style.height);
         } else {
             document.body.classList.remove('no-scroll');
-            console.log('unchecked', document.body.classList);
         }
     });
 });
