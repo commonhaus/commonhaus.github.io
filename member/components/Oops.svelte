36e3efa3c69a721237cbcf9b3a78298ed2b0da78
Erin Schnabel <erinschnabel@gmail.com>
2024-06-10 07:52:09 -0400

    🎨 mention reset button in oops message

--- member/components/Oops.svelte
+++ member/components/Oops.svelte
@@ -1,9 +1,9 @@
 <div class="hold error">
   <h1>Oops!</h1>
   <p><slot></slot></p>
-  <p>Please try again later.</p>
+  <p>The reset button at the bottom of this page may help, or you can try again later.</p>
   <footer>
-    Even better: <a href="https://github.com/commonhaus/automation">open an issue</a>
+    Even better (but optional), <a href="https://github.com/commonhaus/automation">open an issue</a>
     with any errors shown in the browser's debug console (or both!).
   </footer>
 </div>
