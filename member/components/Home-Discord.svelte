3caf69998151cc23fc6f22df33a53ee103e2a8c3
Erin Schnabel <erinschnabel@gmail.com>
2024-08-18 19:17:14 -0400

    update links

--- member/components/Home-Discord.svelte
+++ member/components/Home-Discord.svelte
@@ -4,6 +4,6 @@
 <div class="information">
   <h2>Discord</h2>
 
-  <p>Join the <a href="https://discord.gg/y7Cw42fjqM">Commonhaus Discord server.</a></p>
+  <p>Join the <a href="https://discord.gg/44JdwcUDRf">Commonhaus Discord server.</a></p>
   <p><strong>If you already have a discord account</strong>, log in before clicking the invite link.</p>
 </div>
