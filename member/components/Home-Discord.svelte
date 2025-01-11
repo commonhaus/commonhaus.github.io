b8fa2aea72ced8f27724db46dc7c695881b00585
Erin Schnabel <erinschnabel@gmail.com>
2025-01-10 20:52:37 -0500

    🐛 Update references to discord

--- member/components/Home-Discord.svelte
+++ member/components/Home-Discord.svelte
@@ -4,6 +4,5 @@
 <div class="information">
   <h2>Discord</h2>
 
-  <p>Join the <a href="https://discord.gg/44JdwcUDRf">Commonhaus Discord server.</a></p>
-  <p><strong>If you already have a discord account</strong>, log in before clicking the invite link.</p>
+  <p>Join us on <a href="https://www.commonhaus.org/community/discord.html">Discord</a></p>
 </div>
